7549fa42b4b9e8ced7c8323d9894dde925381e82
Dru Kelly <drukelly@gmail.com>
2019-05-02 18:29:48 -0700

    Movie This

--- liri.js
+++ liri.js
@@ -1,2 +1,40 @@
-require("dotenv").config();
-const keys = require('./keys.js');
+const axios = require('axios')
+require('colors')
+require('dotenv').config()
+const moment = require('moment')
+const nodeSpotify = require('node-spotify-api')
+const keys = require('./keys.js')
+
+let command = process.argv[2]
+let value = process.argv[3]
+
+switch (command) {
+  case 'concert-this' :
+    axios.get('')
+    console.log('concert!')
+    break
+  case 'spotify-this-song' :
+    console.log('spotify!')
+    break
+  case 'movie-this' :
+    // value = value.replace(' ','+').toLowerCase()
+    let queryURL = `https://www.omdbapi.com/?t=${value}&plot=short&apikey=${keys.omdb.apikey}`
+    axios.get(queryURL)
+      .then(response => {
+        console.log(`🎞  ${response.data.Title} 🎞`.yellow)
+        console.log(`Release Date: ${response.data.Released}`.yellow)
+        console.log(`IMDB Rating: ${response.data.Ratings[0].Value}`.yellow)
+        console.log(`🍅 : ${response.data.Ratings[1].Value}`.yellow)
+        console.log(`Country: ${response.data.Country}`.yellow)
+        console.log(`Language: ${response.data.Language}`.yellow)
+        console.log(`Plot: ${response.data.Plot}`.yellow)
+        console.log(`🎭 : ${response.data.Actors}`.yellow)
+      })
+    break
+  case 'do-what-it-says' :
+    console.log('doing!')
+    break
+  default :
+    console.log('Which command is that?')
+    break
+}
