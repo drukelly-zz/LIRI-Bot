4b16029a04fb904b4e8d950e092330d2d08c0ec1
Dru Kelly <drukelly@gmail.com>
2019-05-03 10:20:14 -0700

    Spotify This

--- liri.js
+++ liri.js
@@ -14,15 +14,36 @@ switch (command) {
     console.log('concert!')
     break
   case 'spotify-this-song' :
-    console.log('spotify!')
+    let spotify = new Spotify({
+      id: keys.spotify.id,
+      secret: keys.spotify.secret
+    })
+    if (!value) value = 'Ace of Base - The Sign'
+    spotify.search({
+      type: 'track',
+      query: value
+    }).then(response => {
+      let artists = response.tracks.items[0].artists[0].name
+      let song = response.tracks.items[0].name
+      let album = response.tracks.items[0].album.name
+      let previewURL = response.tracks.items[0].external_urls['spotify']
+      console.log(`Artist(s): ${artists}`.green)
+      console.log(`Song: ${song}`.green)
+      console.log(`Album(s): ${album}`.green)
+      console.log(`Preview URL: ${previewURL}`.green)
+    }).then(error => {
+      if (error) console.log(error)
+    })
     break
   case 'movie-this' :
     value = value.replace(/ /g, '+').toLowerCase()
     let queryURL = `https://www.omdbapi.com/?t=${value}&plot=short&apikey=${keys.omdb.apikey}`
     axios.get(queryURL)
       .then(response => {
+        let convertedDate = moment(new Date(response.data.Released).toISOString())
+        convertedDate = convertedDate.format('ll')
         console.log(`🎞  ${response.data.Title} 🎞`.yellow)
-        console.log(`Release Date: ${response.data.Released}`.yellow)
+        console.log(`Release Date: ${convertedDate}`.yellow)
         console.log(`IMDB Rating: ${response.data.Ratings[0].Value}`.yellow)
         console.log(`🍅 : ${response.data.Ratings[1].Value}`.yellow)
         console.log(`Country: ${response.data.Country}`.yellow)
